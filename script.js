6a981d5c964a3066d3d3027da2be82224866784b
Eduardo Gonzalez <eg9800@gmail.com>
2019-08-01 23:28:13 -0700

    added a empty method when displaying the results so that only one result displays at a time

--- script.js
+++ script.js
@@ -4,14 +4,20 @@
 
   function displayResults(userNameRepos){
     $('#repo-list').empty();
+    $('h2').empty();
+
     console.log(userNameRepos)
+
+    const userNameSearch = $('#username-search').val();
+    //giving the results a title and username
+    $('h2').append(`List of Repos for ${userNameSearch}`)
+
     for (let i = 0; i < userNameRepos.length; i++){
       $('#repo-list').append(
         `<li><h3>${userNameRepos[i].name}</h3></li>
-         <li><h4><a href="${userNameRepos[i].url}">Link!</h4></li>`
-      )
-      //$('#results').removeClass('.hidden');
-    }
+         <li><h4><a target="_blank" href="${userNameRepos[i].html_url}">Link!</h4></li>`
+      )}
+      $('#results').removeClass('hidden');
   }
 
   function searchUsername(userNameSearch){
